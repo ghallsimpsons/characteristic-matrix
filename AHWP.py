b3eddc55948bbaf28f6fb81465b09cf7799bb0ea
Grantland Hall <grantlandhall@berkeley.edu>
2015-01-19 17:18:29 -0800

    Added TwoVector and Stokes representations

--- AHWP.py
+++ AHWP.py
@@ -43,12 +43,24 @@ Phase consideration:
     a_x^2+b_x^2+2a_x*b_x*cos(p), where b_x=(R_1*R_0*(x,y))_x, the
     amplitude of the second-order reflected wave.
     
-
+Things that need to be fixed:
+    For n_x>n_y, R_yx should cause a pi phase shift.
+    When creating stokes vector, it assumes axis offset = 0
     
+Current Examples:
+    Add two parallel vectors - 
+        result = PolarizationVector(1, pi/2) + PolarizationVector(0.1, pi)
+    Add two perpendicular vectors - 
+        result = PolarizationTwoVector( \
+                PolarizationVector(1,0), PolarizationVector(0.2, pi/3))
+    Add two vectors rotated pi/6 -
+        result = PolarizationTwoVector( .1, 0, 0 ) +
+                PolarizationTwoVector( .4, pi/2, pi/6 )
+
 """
 
-from numpy import cos, sin, pi
-import numpy as np
+from numpy import cos, sin, pi, arctan2
+import numpy as n
 
 AHWP_e = 9 # extroardinary index
 AHWP_o = 8 # ordinary index
@@ -83,12 +95,12 @@ class AHWP:
             # from second order reflection.
             
             if (i == len(angles)):
-                # Reflection at AR interface
+                # Reflection at last AR interface
                 R_1 = np.matrix([[0, 0], [0, 0]])
                 R_0 = np.matrix([0, R_oe*cos(angles[i]-angles[i-1])],
                                 [R_oe*cos(angles[i]-angles[i-1]), 0])
             elif (i == 0):
-                # Reflection at AR interface
+                # Reflection at first AR interface
                 R_0 = np.matrix([[0, 0], [0, 0]])
                 R_1 = np.matrix([0, R_oe*cos(angles[i+1]-angles[i])],
                                 [R_oe*cos(angles[i+1]-angles[i]), 0])
@@ -100,3 +112,77 @@ class AHWP:
             second_r = R_1*R_0
             p = 4*pi*freq*(n_e-n_o)*d/c
             transmit = R_1 + second_r
+
+class PolarizationVector:
+    def __init__(self, amplitude, phase):
+        """
+        @param amplitude: Amplitude of wave
+        @param phase: Phase of wave in radians
+        """
+        self.amp = amplitude
+        self.phase = phase
+    def __add__(self, other):
+        a1 = self.amp
+        a2 = other.amp
+        p1 = self.phase
+        p2 = other.phase
+        amp_new = (a1**2) + (a2**2) + (2*a1*a2*cos(p1-p2))
+        phase_new = arctan2( a1*cos(p1) + a2*cos(p2), a1*sin(p1) + a2*sin(p2) )
+        return PolarizationVector(amp_new, phase_new)
+    def power(self):
+        return self.amp**2
+
+class StokesVector:
+    def __init__(self, *args):
+        if len(args) == 4:
+            # stokes repr
+            self.I = args[0]
+            self.Q = args[1]
+            self.U = args[2]
+            self.V = args[3]
+        elif len(args) == 2:
+            x = args[0]
+            y = args[1]
+            I = x.power() + y.power()
+            Q = x.power() - y.power()
+            U = 2*x.amp*y.amp*(cos(x.phase)*cos(y.phase)+sin(x.phase)*sin(y.phase))
+            V = 2*x.amp*y.amp*(cos(x.phase)*sin(y.phase)+sin(x.phase)*cos(y.phase))
+
+class PolarizationTwoVector:
+    """
+    This class is similar to a Stokes Vector,
+    but designed to allow phase-offset vector addition.
+    """
+    def __init__(self, vector_x, vector_y, axis_orientation = 0):
+        self.v_x = vector_x
+        self.v_y = vector_y
+        self.orientation = axis_orientation
+
+    def rot(self, rad):
+        """Return rotated representation of self"""
+        x_to_rot_x = vector_x.amp*cos(rad)
+        x_to_rot_y = -1*vecrot_x.amp*sin(rad)
+        y_to_rot_x = vector_y.amp*cos(sin)
+        y_to_rot_y = vector_y.amp*cos(rad)
+        new_x = PolarizationVector(x_to_rot_x, vector_x.phase) + \
+            PolarizationVector(y_to_rot_x, vector_y.phase)
+        new_y = PolarizationVector(x_to_rot_y, vector_x.phase) + \
+            PolarizationVector(y_to_rot_y, vector_y.phase)
+        return PolarizationTwoVector(new_x, new_y, self.orientation + rad)
+    
+    def _rot(self, rad):
+        """Rotate self"""
+        self = self.rot(rad)
+    
+    def __add__(self, other):
+        """
+        Returns two-vector in the orientation of the first two-vector
+        """
+        offset = other.orientation - self.orientation
+        other_r = other.rot(-offset)
+        return PolarizationTwoVector( self.v_x + other_r.v_x, 
+                            self.v_y + other_r.v_y, self.orientation )
+
+    def stokes(self):
+        unrotated = self.rot( -self.orientation )
+        return StokesVector(self.v_x, self.v_y)
