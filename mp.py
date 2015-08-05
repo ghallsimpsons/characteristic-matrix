0303a95def9f26cf3c861bfeec2456762b4d55af
Grantland Hall <grantlandhall@grantlandhall.com>
2015-08-04 18:24:28 -0700

    add support for modules and globals in mp

--- mp.py
+++ mp.py
@@ -11,6 +11,17 @@ def mpexec(kernel, param, values, **kwargs):
         raise RuntimeError(
             "Sweep parameter '{}' can not be passed as a kwarg.".format(
             param))
+    if "globals" in kwargs.keys():
+        myglobals = kwargs['globals']
+        del kwargs['globals']
+        print "Pi: {}".format(myglobals['pi'])
+    else:
+        myglobals = None
+    if "modules" in kwargs.keys():
+        mymodules = kwargs['modules']
+        del kwargs['modules']
+    else:
+        mymodules = ()
     nprocs = cpu_count()-1
     job_server = pp.Server()
     job_server.set_ncpus(nprocs)
@@ -24,7 +35,8 @@ def mpexec(kernel, param, values, **kwargs):
         ext_kwargs.update({param: v})
         def job(kernel, kwargs):
             return kernel(**kwargs)
-        jobs.append(job_server.submit(job, (kernel, ext_kwargs)))
+        jobs.append(job_server.submit(job, (kernel, ext_kwargs),
+                modules=mymodules, globals=myglobals))
     results = [result() for result in jobs]
     end_time = time.time()
     print "elapsed: {}".format((end_time-start_time))
